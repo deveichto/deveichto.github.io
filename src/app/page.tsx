f44a5672363d01e34a723b9cc930e7d95ff3690a
Dev Eichto <eichto.developer@gmail.com>
2025-01-25 10:44:15 +0800

    test layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
 export default function Home() {
   return (
-    <div className="w-full flex text-center justify-center">
+    <div className="w-full flex flex-col text-center justify-center mt-20">
       <h1 className="font-bold text-xl">Mark Portolio</h1>
 
-      <p className="text-sm">Content will soon to upload....</p>
+      <p className="text-sm mt-5">Content will soon to upload....</p>
     </div>
   );
 }
